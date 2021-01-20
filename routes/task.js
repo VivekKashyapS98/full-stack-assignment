db879a703780a6dfc989be9533553021a6688d51
Vivek Kashyap <vivekyeskashyap@gmail.com>
2021-01-20 13:28:57 +0530

    bug fixes

--- routes/task.js
+++ routes/task.js
@@ -13,8 +13,8 @@ router
   .route("/:id")
   .get(loginRequired, ensureCorrectUser, getTasks)
   .post(loginRequired, ensureCorrectUser, addTask);
-router.delete("/id/:id2", loginRequired, ensureCorrectUser, removeTask);
-router.post("/id/complete/:id2", loginRequired, ensureCorrectUser, setComplete);
-router.post("/id/notes/id2", loginRequired, ensureCorrectUser, addNotes);
+router.delete("/:id/:id2", loginRequired, ensureCorrectUser, removeTask);
+router.post("/:id/complete/:id2", loginRequired, ensureCorrectUser, setComplete);
+router.post("/:id/notes/id2", loginRequired, ensureCorrectUser, addNotes);
 
 module.exports = router;
